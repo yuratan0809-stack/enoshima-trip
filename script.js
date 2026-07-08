04a66d7f3cb576a7c3b62ab5ae0b3f48db210a73
yuratan0809-stack <y.uratan0809@gmail.com>
2026-07-09 01:43:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 const fadeElements = document.querySelectorAll(
 ".card, .spot, .photo-card, .event"
 );
+const sections = document.querySelectorAll("section");
+const menuLinks = document.querySelectorAll(".menu-link");
+
+window.addEventListener("scroll",()=>{
+
+  let current = "";
+
+  sections.forEach(section=>{
+    const top = section.offsetTop - 150;
+
+    if(scrollY >= top){
+      current = section.id;
+    }
+  });
+
+
+  menuLinks.forEach(link=>{
+    link.classList.remove("active");
+
+    if(link.getAttribute("href") === "#" + current){
+      link.classList.add("active");
+    }
+  });
+
+});
